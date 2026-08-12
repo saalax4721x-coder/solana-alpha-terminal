671a8d55b58c377d240ed5fcbb7eb6cc7c9a0fdc
saalax4721x-coder <saalax4721x@gmail.com>
2026-08-13 00:33:25 +0300

    Start dual-source social intelligence worker

--- apps/api/src/launcher.js
+++ apps/api/src/launcher.js
@@ -1,10 +1,5 @@
 import "./server.js";
 import "./spam-worker.js";
-import { startIntelligence } from "./intelligence-worker.js";
+import { startSocialWorker } from "./social-worker.js";
 
-const databaseUrl = process.env.DATABASE_URL;
-if (databaseUrl) {
-  const { default: pg } = await import("pg");
-  const pool = new pg.Pool({ connectionString: databaseUrl, ssl: { rejectUnauthorized: false }, max: 10, idleTimeoutMillis: 30000, connectionTimeoutMillis: 10000 });
-  startIntelligence(pool);
-}
+startSocialWorker();
