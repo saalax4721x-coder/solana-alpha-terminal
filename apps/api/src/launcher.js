d70293fe5d8b9f9d870075bc36aba89348987025
saalax4721x-coder <saalax4721x@gmail.com>
2026-08-13 00:29:22 +0300

    Start production intelligence worker with API

--- apps/api/src/launcher.js
+++ apps/api/src/launcher.js
@@ -1,2 +1,10 @@
 import "./server.js";
 import "./spam-worker.js";
+import { startIntelligence } from "./intelligence-worker.js";
+
+const databaseUrl = process.env.DATABASE_URL;
+if (databaseUrl) {
+  const { default: pg } = await import("pg");
+  const pool = new pg.Pool({ connectionString: databaseUrl, ssl: { rejectUnauthorized: false }, max: 10, idleTimeoutMillis: 30000, connectionTimeoutMillis: 10000 });
+  startIntelligence(pool);
+}
